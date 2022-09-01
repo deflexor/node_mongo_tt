c4b8e2e9fd787ba3a49fb704c7e9e7707d16ed4b
Dmitriy Knyazev <deflexor@yandex.ru>
2022-09-01 14:00:15 +0300

    ...

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
-'use strict';
-import * as dotenv from 'dotenv'
-dotenv.config()
-import { MongoClient } from 'mongodb'
+import * as dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 import { loadJSONFiles } from './loadJSONFiles.js';
 
+dotenv.config();
 
-// Connection URL
+// Connection Data
 const url = process.env.DB_URL;
 const client = new MongoClient(url);
-
-// Database Name
 const dbName = process.env.DB_NAME;
+const col1 = process.env.COLLECTION1_NAME
+const col2 = process.env.COLLECTION2_NAME
+const col3 = 'result'
 
 async function main() {
-  // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
   await loadJSONFiles(db);
-  // the following code examples can be pasted here...
-
+  // Task part 3, aggregate coords
+  const $coordsAgg = await db.collection(col1).aggregate([
+    { $project: { 
+      country : 1,
+      lng: { $arrayElemAt: [ "$location.ll", 0 ] },
+      lat: { $arrayElemAt: [ "$location.ll", 1 ] } } },
+    { $group: { "_id": "$country", longitude: { $push: "$lng" }, latitude: { $push: "$lat" } }}
+  ]).toArray();
+  let coordsByCountry = new Map();
+  $coordsAgg.forEach((row) => {
+    coordsByCountry[row._id] = { latitude: row.latitude, longitude: row.longitude };
+  })
+  // Task part 4, students difference
+  const $sdiffAgg = await db.collection(col1).aggregate([
+    { $lookup:
+      {
+         from: col2,
+         localField: "country",
+         foreignField: "country",
+         as: "col2"
+      }
+    },
+    { $project: { country: 1, students: 1, col2: 1 } },
+    { $group:
+       { "_id": "$country", 
+        col2: { $first: "$col2.overallStudents"},
+        detStudents: { "$sum": { "$sum": "$students.number"} } }
+    },
+    {
+      $addFields:{
+        diffStundents: { $subtract: ["$detStudents", { $first: "$col2" }] }
+      }
+   }
+  ]).toArray();
+  // Task part 5: Find documents count by countries
+  const $docsAgg = await db.collection(col1).aggregate([
+    {"$group": { _id: "$country", count: { $sum:1 } } },
+    { $sort: { "count": -1 } }
+  ]).toArray();
+  let countByCountry = new Map();
+  $docsAgg.forEach((row) => {
+    countByCountry[row._id] = row.count;
+  })
+  //  console.log($coordsAgg);
+  // Task 6: Write result to third collection to the current database
+  const result = $coordsAgg.map((row) => {
+    return {
+      _id: row._id,
+      allDiffs: $sdiffAgg,
+      count: countByCountry[row._id],
+      longitude: coordsByCountry[row._id].longitude,
+      latitude: coordsByCountry[row._id].latitude
+    };
+  });
+  // console.log(result)
+  await db.collection(col3).drop()
+  await db.collection(col3).insertMany(result)
   return 'done.';
 }
-
-
 
 main()
   .then(console.log)
--- loadJSONFiles.js
+++ loadJSONFiles.js
@@ -1,50 +1,64 @@
-'use strict';
 import * as fs from 'fs';
+import * as dotenv from 'dotenv';
 import { exit } from 'process';
 
-export async function loadJSONFiles(db) {
-  const detailsCollection = db.collection('details');
-  const summaryCollection = db.collection('summary');
-  try {
-    const detailsJson = loadJSON("./first.json", idF, addIDsToJSON(['country', 'city', 'name']));
-    const summaryJson = loadJSON("./second.json", fixSummaryJSON, addIDsToJSON(['country']));
-    let insertResult = await detailsCollection.insertMany(detailsJson);
-    console.log(`Loaded detailsJson`);
-    insertResult = await summaryCollection.insertMany(summaryJson);
-    console.log(`Loaded summaryJson`);
-  } catch (e) {
-    console.error(`Error loading json into database; ${e}`);
-  }
+dotenv.config();
+
+const colDetailsName = process.env.COLLECTION1_NAME
+const colSummaryName = process.env.COLLECTION2_NAME
+
+function idF(x) {
+  return x;
 }
 
-export function idF(x) {
-  return x
-}
-
-export function fixSummaryJSON(text) {
-  let r = text.replaceAll('country', '"country"');
-  r = r.replaceAll("'", '"');
-  return r
-}
-
-export const addIDsToJSON = (idFlds = []) => (json) => {
-  if(idFlds.length === 0) return json;
-  if(!Array.isArray(json)) return json;
-  for(let obj of json) {
-    if(obj['_id']) continue;
-    obj['_id'] = idFlds.map(f => obj[f] || 'x').join('_')
-  }
-  return json
-}
-
-export function loadJSON(filename, filterTextF = idF, modifyJSONF = idF) {
+function loadJSON(filename, filterTextF = idF, modifyJSONF = idF) {
+  let json;
   try {
     const fileData = filterTextF(fs.readFileSync(filename).toString('utf8'));
-    const json = JSON.parse(fileData);
+    json = JSON.parse(fileData);
     modifyJSONF(json);
     return json;
   } catch (e) {
     console.error(`failed to load ${filename}! ${e}`);
-    process.exit();
+    exit();
+  } finally {
+    return json;
+  }
+}
+
+function fixSummaryJSON(text) {
+  let r = text.replaceAll('country', '"country"');
+  r = r.replaceAll("'", '"');
+  return r;
+}
+
+const addIDsToJSON = (idFlds = []) => (json) => {
+  if (idFlds.length === 0) return json;
+  if (!Array.isArray(json)) return json;
+  json.forEach((obj) => {
+    if (!obj._id) {
+      obj._id = idFlds.map((f) => obj[f] || 'x').join('_');
+    }
+  });
+  return json;
+};
+
+export async function loadJSONFiles(db) {
+  const detailsCollection = db.collection(colDetailsName);
+  const summaryCollection = db.collection(colSummaryName);
+  try {
+    const detailsJson = loadJSON('./first.json', idF, addIDsToJSON(['country', 'city', 'name']));
+    const summaryJson = loadJSON('./second.json', fixSummaryJSON, addIDsToJSON(['country']));
+    await detailsCollection.insertMany(detailsJson);
+    console.log('Loaded detailsJson');
+    await summaryCollection.insertMany(summaryJson);
+    console.log('Loaded summaryJson');
+  } catch (e) {
+    if (`${e}`.indexOf('E11000 duplicate key') !== -1) {
+      // colleactions already loaded, ok to skip
+    } else {
+      console.error(`Error loading json into database; ${e}`);
+      exit();
+    }
   }
 }
